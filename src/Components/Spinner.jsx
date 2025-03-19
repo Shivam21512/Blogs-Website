b0f1fb4ed6f5745459e2df205f98be32ff82f904
Shivam Ugale <141569702+Shivam21512@users.noreply.github.com>
2025-03-19 13:28:34 +0530

    Update Spinner.jsx

--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -10,36 +10,3 @@ const Spinner = () => {
 
 
 export default Spinner
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react'
-
-// const Spinner = () => {
-//   return (
-//     <div className='flex justify-center items-center flex-col '>
-//       <div className='spinner'></div>
-//       <p className='font-bold text-lg'>Loading...</p>
-//     </div>
-//   )
-// }
-
-// export default Spinner
