4890b7ce91fcdbb36cd0f0df6d7e09903d16dc4f
Shivam Ugale <141569702+Shivam21512@users.noreply.github.com>
2025-03-31 14:51:20 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -17,35 +17,3 @@ const Home = () => {
 }
 
 export default Home 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react'
-// import Header from '../Components/Header'
-// import Blogs from '../Components/Blogs'
-// import Pagination from '../Components/Pagination'
-
-
-// const Home = () => {
-//     return (
-//         <div>
-//             <Header />
-//             <Blogs />
-//             <Pagination />
-//         </div> 
-//     )
-// }
-
-// export default Home
